e6d3a75033dc99be5cafa8b1a72452c75a7298a3
Michael Kronovet <mkronovet@gmail.com>
2018-02-10 01:29:43 -0500

    more web stuff

--- ProfWebsite/DoppelGangerWebsite/DoppelGangerWebsite/urls.py
+++ ProfWebsite/DoppelGangerWebsite/DoppelGangerWebsite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
+from DoppelGangerWebsite.views import MainPage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', MainPage.as_view(), name="main")
--- ProfWebsite/DoppelGangerWebsite/database/models.py
+++ ProfWebsite/DoppelGangerWebsite/database/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Professors(models.Model):
+    profName = models.CharField(max_length=50)
+    profPic = models.CharField(default="", max_length=400)
+
+class User(models.Model):
+    userPic = models.CharField(default="", max_length=400)
